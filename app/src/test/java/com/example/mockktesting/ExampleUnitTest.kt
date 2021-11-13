f042691756824a44d9de356f256568d5ac1569d6
Brad Veenvliet <bveenvliet@gmail.com>
2021-11-13 14:21:04 -0500

    with mockk 1.9.3 the andThenAnswer doesn't work

--- app/src/test/java/com/example/mockktesting/ExampleUnitTest.kt
+++ app/src/test/java/com/example/mockktesting/ExampleUnitTest.kt
@@ -41,40 +41,41 @@ class ExampleUnitTest {
                 any(),
                 any()
             )
-        } answers { "2" } andThenAnswer { "4" } andThenAnswer { "15" }
-        assertEquals("2", mathFunctionsMock.add(1, 1))
-        assertEquals("4", mathFunctionsMock.add(2, 2))
-        assertEquals("15", mathFunctionsMock.add(10, 5))
+        } answers { "2!" } // andThenAnswer { "4!" } andThenAnswer { "15!" }
+        assertEquals("2!", mathFunctionsMock.add(1, 1))
+//        assertEquals("4!", mathFunctionsMock.add(2, 2))
+//        assertEquals("15!", mathFunctionsMock.add(10, 5))
+//        assertEquals("15!", mathFunctionsMock.add(0, 0))
     }
 
     @Test
     fun testWithMock2() {
         // using a spy... so only override specific values 1, 1
-        every { mathFunctionsSpy.add(1, 1) } answers { "App" } andThenAnswer { "Apple" }
+        every { mathFunctionsSpy.add(1, 1) } answers { "App" } // andThenAnswer { "Apple" }
         assertEquals("App", mathFunctionsSpy.add(1, 1))
-        assertEquals("Apple", mathFunctionsSpy.add(1, 1))
-        assertEquals("4", mathFunctionsSpy.add(2, 2))
-        assertEquals("15", mathFunctionsSpy.add(10, 5))
+//        assertEquals("Apple", mathFunctionsSpy.add(1, 1))
+//        assertEquals("4", mathFunctionsSpy.add(2, 2))
+//        assertEquals("15", mathFunctionsSpy.add(10, 5))
     }
 
     @Test
     fun testWithMock3() {
         // using a mock... so it will return bar then always bat
-        every { mathFunctionsMock.add(any(), any()) } answers { "Bar" } andThenAnswer { "Bat" }
+        every { mathFunctionsMock.add(any(), any()) } answers { "Bar" } // andThenAnswer { "Bat" }
         assertEquals("Bar", mathFunctionsMock.add(1, 1))
-        assertEquals("Bat", mathFunctionsMock.add(1, 1))
-        assertEquals("Bat", mathFunctionsMock.add(2, 2))
-        assertEquals("Bat", mathFunctionsMock.add(10, 5))
+//        assertEquals("Bat", mathFunctionsMock.add(1, 1))
+//        assertEquals("Bat", mathFunctionsMock.add(2, 2))
+//        assertEquals("Bat", mathFunctionsMock.add(10, 5))
     }
 
     @Test
     fun testWithMock4() {
         // using a mock... so only override specific values 1, 1
-        every { mathFunctionsMock.add(1, 1) } answers { "Bar" } andThenAnswer { "Bat" }
+        every { mathFunctionsMock.add(1, 1) } answers { "Bar" } // andThenAnswer { "Bat" }
         assertEquals("Bar", mathFunctionsMock.add(1, 1))
-        assertEquals("Bat", mathFunctionsMock.add(1, 1))
-        assertEquals("", mathFunctionsMock.add(2, 2))
-        assertEquals("", mathFunctionsMock.add(10, 5))
+//        assertEquals("Bat", mathFunctionsMock.add(1, 1))
+//        assertEquals("", mathFunctionsMock.add(2, 2))
+//        assertEquals("", mathFunctionsMock.add(10, 5))
     }
 
 }
